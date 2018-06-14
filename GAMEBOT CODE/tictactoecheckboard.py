e1bc1667ae79d8936d3ad0ee6a1515464f4fbfa9
catswithknives <39057984+catswithknives@users.noreply.github.com>
2018-06-14 10:06:35 -0400

    Add files via upload
    updated detection code

--- GAMEBOT CODE/tictactoecheckboard.py
+++ GAMEBOT CODE/tictactoecheckboard.py
@@ -1,8 +1,8 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
-#GPIO.setmode(GPIO.BCM)
-outputs = [18, 23, 24, 25, 12, 16, 20, 21] #only use 3, put the rest in comments
-inputs = [4, 17, 27, 22, 5, 6, 13, 19] #pick 3 again, put rest in comments
+GPIO.setmode(GPIO.BOARD)
+outputs = [12, 16, 18] #22, 32, 36, 38, 40]
+inputs = [7, 11, 13] #15, 29, 31, 33, 35]
 '''boardState = [
     #a  b  c  d  e  f  g  h
     [1, 0, 1, 0, 1, 0, 1, 0], #8
@@ -23,25 +23,25 @@ boardState = [
 ]
 
 for outPin in range (0, 3): # 3 by 3 grid
-    #GPIO.setup(outputs[outPin], GPIO.OUT)
-    print (outputs[outPin])
+    GPIO.setup(outputs[outPin], GPIO.OUT)
+    print (outputs[outPin], "Poi")
 
 for inPin in range (0,3): # 3 by 3 grid
-    #GPIO.setup(inputs[inPin], GPIO.IN)
-    print (inputs[inPin])
+    GPIO.setup(inputs[inPin], GPIO.IN)
+    print (inputs[inPin], "Koi")
 
 
-#print (boardState[1][2])
+#print (boardState[yuudachi][poi]) test
 
 def WhereAreSquares(): #transmitters need to be horizontal & pickups need to be vertical when wired
     for send in range (0, 3): # 3 by 3 grid
         GPIO.output([outputs], 1)
         for receive in range (0, 3): # 3 by 3 grid
             if GPIO.input([inputs]):
-                #boardState[send][receive] = 1
+                boardState[send][receive] = 1
                 print (boardState[send][receive])
             else:
-                #boardState[send][receive] = 0
+                boardState[send][receive] = 0
                 print (boardState[send][receive])
                 
                 
