7c194d4faad0f742b45e8a08c68f88a18939c232
noahb3683 <39033497+noahb3683@users.noreply.github.com>
2018-06-14 11:10:22 -0400

    Add files via upload
    noah is incompetent. added tictactoe changes to working checkboard code. should be fixed

--- GAMEBOT CODE/tictactoecheckboard.py
+++ GAMEBOT CODE/tictactoecheckboard.py
@@ -1,47 +1,43 @@
 import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
-outputs = [12, 16, 18] #22, 32, 36, 38, 40]
-inputs = [7, 11, 13] #15, 29, 31, 33, 35]
+outputs = [12, 16, 18] # 22, 32, 36, 38, 40]
+inputs = [7, 11, 13] # 15, 29,31, 33, 35]
 '''boardState = [
-    #a  b  c  d  e  f  g  h
-    [1, 0, 1, 0, 1, 0, 1, 0], #8
-    [0, 1, 0, 1, 0, 1, 0, 1], #7
-    [1, 0, 1, 0, 1, 0, 1, 0], #6
-    [0, 0, 0, 0, 0, 0, 0, 0], #5
-    [0, 0, 0, 0, 0, 0, 0, 0], #4
-    [0, 1, 0, 1, 0, 1, 0, 1], #3
-    [1, 0, 1, 0, 1, 0, 1, 0], #2
-    [0, 1, 0, 1, 0, 1, 0, 1]  #1
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1],
 ]'''
-#legacy checkers array
-
 boardState = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
 ]
 
-for outPin in range (0, 3): # 3 by 3 grid
+for outPin in range(0, 3):
     GPIO.setup(outputs[outPin], GPIO.OUT)
-    print (outputs[outPin], "Poi")
+    print (outputs[outPin], "poi")
 
-for inPin in range (0,3): # 3 by 3 grid
+for inPin in range(0, 3):
     GPIO.setup(inputs[inPin], GPIO.IN)
-    print (inputs[inPin], "Koi")
+    print (inputs[inPin], "koi")
 
-
-#print (boardState[yuudachi][poi]) test
-
-def WhereAreSquares(): #transmitters need to be horizontal & pickups need to be vertical when wired
-    for send in range (0, 3): # 3 by 3 grid
-        GPIO.output([outputs], 1)
-        for receive in range (0, 3): # 3 by 3 grid
-            if GPIO.input([inputs]):
+def WhereAreSquares():
+    for send in range(0, 3):
+        GPIO.output(outputs[send], 1)
+        for receive in range(0, 3):
+            if GPIO.input(inputs[receive]):
                 boardState[send][receive] = 1
-                print (boardState[send][receive])
+                print ("LASERBOI HAS NAILED HIS TARGET")
             else:
                 boardState[send][receive] = 0
-                print (boardState[send][receive])
-                
-                
+                print ("nobody home & laserboi is a sad sad laserboi... :(")
+WhereAreSquares()
+for poicon in boardState:
+    print (poicon)
+GPIO.cleanup()
