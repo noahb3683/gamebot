8a13a97b4f97dd42d74217df26d53b79a5dc2952
noahb3683 <39033497+noahb3683@users.noreply.github.com>
2018-06-20 10:00:44 -0400

    Add files via upload

--- hardtimeshardcode.py
+++ hardtimeshardcode.py
@@ -28,30 +28,39 @@ for inPin in range(0, 3):
     print (inputs[inPin], "koi")
 
 def WhereAreSquares():
-    GPIO.output(31, 1)
-    if GPIO.input(12):
+    GPIO.output(12, 1)
+    if GPIO.input(31):
         print ("31, 12")
-    if GPIO.input(16):
-        print ("31, 16")
-    if GPIO.input(18):
-        print ("31, 18")
-    GPIO.output(31, 0)
-    GPIO.output(33, 1)
-    if GPIO.input(12):
+        boardState[2][0] = 1
+    if GPIO.input(33):
         print ("33, 12")
-    if GPIO.input(16):
-        print ("33, 16")
-    if GPIO.input(18):
-        print ("33, 18")
-    GPIO.output(33, 0)
-    GPIO.output(35, 1)
-    if GPIO.input(12):
+        boardState[2][1] = 1
+    if GPIO.input(35):
         print ("35, 12")
-    if GPIO.input(16):
+        boardState[2][2] = 1
+    GPIO.output(12, 0)
+    GPIO.output(16, 1)
+    if GPIO.input(31):
+        print ("31, 16")
+        boardState[1][0] = 1
+    if GPIO.input(33):
+        print ("33, 16")
+        boardState[1][1] = 1
+    if GPIO.input(35):
         print ("35, 16")
-    if GPIO.input(18):
+        boardState[1][2] = 1
+    GPIO.output(16, 0)
+    GPIO.output(18, 1)
+    if GPIO.input(31):
+        print ("31, 18")
+        boardState[0][0] = 1
+    if GPIO.input(33):
+        print ("33, 18")
+        boardState[0][1] = 1
+    if GPIO.input(35):
         print ("35, 18")
-    GPIO.output(35, 0)
+        boardState[0][2] = 1
+    GPIO.output(18, 0)
 
         
 WhereAreSquares()
