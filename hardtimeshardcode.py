85c7a11e0e8033e8d6c1ac853214958b8e8862cc
noahb3683 <39033497+noahb3683@users.noreply.github.com>
2018-06-20 09:51:46 -0400

    Add files via upload

--- hardtimeshardcode.py
+++ hardtimeshardcode.py
@@ -29,27 +29,27 @@ for inPin in range(0, 3):
 
 def WhereAreSquares():
     GPIO.output(31, 1)
-    if GPIO.input(12)
+    if GPIO.input(12):
         print ("31, 12")
-    if GPIO.input(16)
+    if GPIO.input(16):
         print ("31, 16")
-    if GPIO.input(18)
+    if GPIO.input(18):
         print ("31, 18")
     GPIO.output(31, 0)
     GPIO.output(33, 1)
-    if GPIO.input(12)
+    if GPIO.input(12):
         print ("33, 12")
-    if GPIO.input(16)
+    if GPIO.input(16):
         print ("33, 16")
-    if GPIO.input(12)
+    if GPIO.input(18):
         print ("33, 18")
     GPIO.output(33, 0)
     GPIO.output(35, 1)
-    if GPIO.input(12)
+    if GPIO.input(12):
         print ("35, 12")
-    if GPIO.input(12)
+    if GPIO.input(16):
         print ("35, 16")
-    if GPIO.input(12)
+    if GPIO.input(18):
         print ("35, 18")
     GPIO.output(35, 0)
 
